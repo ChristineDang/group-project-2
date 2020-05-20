0e868ff84aae70c764efd41d1fb1e3e38821379f
Mike Vong <mv910@hotmail.com>
2020-05-20 16:19:48 -0700

    added routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,24 @@
 var db = require("../models");
 
 module.exports = function(app) {
-    // Get all examples
-    app.get("/api/examples", function(req, res) {
-        db.Example.findAll({}).then(function(dbExamples) {
-            res.json(dbExamples);
+    // Get all UserInfo
+    app.get("/api/userInfo", function(req, res) {
+        db.UserInfo.findAll({}).then(function(dbUserInfo) {
+            res.json(dbUserInfo);
         });
     });
 
-    // Create a new example
-    app.post("/api/examples", function(req, res) {
-        db.Example.create(req.body).then(function(dbExample) {
-            res.json(dbExample);
+    // Create a new UserInfo
+    app.post("/api/userInfo", function(req, res) {
+        db.UserInfo.create(req.body).then(function(dbUserInfo) {
+            res.json(dbUserInfo);
         });
     });
 
-    // Delete an example by id
-    app.delete("/api/examples/:id", function(req, res) {
-        db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-            res.json(dbExample);
+    // Delete an UserInfo by id
+    app.delete("/api/userInfo/:id", function(req, res) {
+        db.UserInfo.destroy({ where: { id: req.params.id } }).then(function(dbUserInfo) {
+            res.json(dbUserInfo);
         });
     });
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,9 +2,9 @@ var db = require("../models");
 
 module.exports = function(app) {
     // Load index page
-    app.get("/", function(req, res) {
-        db.Example.findAll({}).then(function(dbExamples) {
-            res.render("index", {
+    app.get("/userInfo", function(req, res) {
+        db.UserInfo.findAll({}).then(function(dbExamples) {
+            res.render("eligibility", {
                 msg: "Welcome!",
                 examples: dbExamples
             });
@@ -12,10 +12,10 @@ module.exports = function(app) {
     });
 
     // Load example page and pass in an example by id
-    app.get("/example/:id", function(req, res) {
+    app.get("/userInfo/:id", function(req, res) {
     // eslint-disable-next-line prettier/prettier
-        db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-            res.render("example", {
+        db.UserInfo.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+            res.render("eligibility", {
                 example: dbExample
             });
         });
